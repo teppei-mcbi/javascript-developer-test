866e91a8f0878de8c5fbcacdf591e955667fdd0d
teppei-mcbi <teppei.nishimura@mcbi.com.au>
2021-12-24 22:30:52 +1030

    check what results will be returned by httpGet()

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,32 @@
 const { httpGet } = require('./mock-http-interface');
 
+/**
+ * 
+ * @param {*} urls - array of strings
+ */
 const getArnieQuotes = async (urls) => {
+  const results = [];
+  
   // TODO: Implement this function.
-  // return results;
+  if (Array.isArray(urls)) {
+    for (const url of urls) {
+      console.log(`[URL]: ${url}`)
+
+      const httpGetResult = await httpGet(url)
+        .then(value => {
+          console.log(`success value: ${JSON.stringify(value)}`)
+          return value;
+        })
+        .catch(err => {
+          console.error(`error: ${JSON.stringify(err)}`)
+          return err
+        });
+
+      // console.log(`result: ${JSON.stringify(httpGetResult)}`)
+
+    }
+  }
+  return results;
 };
 
 module.exports = {
