1ba0577634b94deb7c1255c85728a08ca07fceab
teppei-mcbi <teppei.nishimura@mcbi.com.au>
2021-12-24 22:52:58 +1030

    add 'start' script to run & check results

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -5,15 +5,29 @@ const { httpGet } = require('./mock-http-interface');
  * @param {*} urls - array of strings
  */
 const getArnieQuotes = async (urls) => {
+  console.log(`----------- getArnieQuotes ----------`)
   const results = [];
-  
+
   // TODO: Implement this function.
   if (Array.isArray(urls)) {
     for (const url of urls) {
       console.log(`[URL]: ${url}`)
 
-      const httpGetResult = await httpGet(url)
-        .then(value => {
+      const quote = await httpGet(url)
+        .then(httpResult => {
+
+          const statusCode = httpResult.status;
+          const msgJson = JSON.parse(httpResult.body);
+
+          switch (statusCode) {
+            case 200:
+              return { 'Arnie Quote': msgJson.message };
+        
+            case 500:
+            default:
+              return { 'FAILURE': msgJson.message}
+          }
+
           console.log(`success value: ${JSON.stringify(value)}`)
           return value;
         })
@@ -22,12 +36,25 @@ const getArnieQuotes = async (urls) => {
           return err
         });
 
-      // console.log(`result: ${JSON.stringify(httpGetResult)}`)
+      console.log(`--- quote: ${JSON.stringify(quote)}`)
 
+      results.push(quote);
     }
+
+    console.log(`results: ${JSON.stringify(results)}`)
+
   }
   return results;
 };
+
+const urls = [
+  'http://www.smokeballdev.com/arnie0',
+  'http://www.smokeballdev.com/arnie1',
+  'http://www.smokeballdev.com/arnie2',
+  'http://www.smokeballdev.com/arnie3',
+];
+
+getArnieQuotes(urls);
 
 module.exports = {
   getArnieQuotes,
